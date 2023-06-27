f51f70179edfb0a035f4de3666b4519605016d45
Alex <memessensei@gmail.com>
2023-06-27 05:21:09 +0300

    Added header and footer

---
+++ src/app/footer.jsx
@@ -0,0 +1,10 @@
+import styles from "@/footer.module.css";
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div>Вопросы-ответы</div>
+            <div>О нас</div>
+        </footer>
+    );
+}
